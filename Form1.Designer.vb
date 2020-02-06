f33f0cc7342452c00e128a8b79ee89114830257d
bmartinezdiaz <bmartinezdiaz@gmail.com>
2020-02-06 15:42:57 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(67, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(136, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Fecha internacional:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(209, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(187, 22)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(402, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "convertir a "
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(503, 32)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(234, 22)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(209, 75)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(143, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Convertir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(40, 131)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(162, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Cargar XML"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(209, 131)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(699, 22)
+        Me.TextBox3.TabIndex = 6
+        Me.TextBox3.Text = "C:\Temp\archivos_xml\2019-05-29.401.2.xml"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(40, 182)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(868, 359)
+        Me.RichTextBox1.TabIndex = 7
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1010, 606)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Xml
+'modulo ParsingUsingXmlDocument
+imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = DateTime.Parse(TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim m_xmld As XmlDocument
+            Dim m_nodelist As XmlNodeList
+            Dim m_node As XmlNode
+
+            'Creamos el "Document"
+            m_xmld = New XmlDocument()
+
+            'Cargamos el archivo
+            m_xmld.Load(TextBox3.Text)
+
+            'Obtenemos la lista de los nodos "name"
+            m_nodelist = m_xmld.SelectNodes("transactionSet/trans/trLines/trLine")
+            Dim No As Integer = 0
+
+            'Iniciamos el ciclo de lectura
+            For Each m_node In m_nodelist
+                'Obtenemos el atributo del codigo
+                'Dim mCodigo = m_node.Attributes.GetNamedItem("date").Value
+
+                If m_node.ChildNodes.Item(7).Name = "trlLineTot" Then
+                    If CDec(m_node.ChildNodes.Item(7).InnerText) < 0 Then
+                        RichTextBox1.Text = RichTextBox1.Text + m_node.ChildNodes.Item(10).InnerText + vbNewLine
+                    End If
+                ElseIf m_node.ChildNodes.Item(8).Name = "trlLineTot" Then
+                    If CDec(m_node.ChildNodes.Item(8).InnerText) < 0 Then
+                        RichTextBox1.Text = RichTextBox1.Text + m_node.ChildNodes.Item(10).InnerText + vbNewLine
+                    End If
+                Else
+                    RichTextBox1.Text = RichTextBox1.Text + "No se encontro"
+                End If
+
+                'No = No + 1
+
+                'If CDec(m_node.ChildNodes.Item(7).InnerText) < 0 Then
+                '    RichTextBox1.Text = RichTextBox1.Text +
+                '                       No.ToString + " - " + m_node.ChildNodes.Item(0).InnerText + ", " + _
+                '                                         m_node.ChildNodes.Item(1).InnerText + ", " + _
+                '                                         m_node.ChildNodes.Item(2).InnerText + ", " + _
+                '                                         m_node.ChildNodes.Item(3).InnerText + ", " + _
+                '                                         m_node.ChildNodes.Item(4).InnerText + ", " + _
+                '                                         m_node.ChildNodes.Item(5).InnerText + ", " + _
+                '                                         m_node.ChildNodes.Item(6).InnerText + ", " + _
+                '                                         m_node.ChildNodes.Item(7).InnerText + ", " + _
+                '                                         m_node.ChildNodes.Item(8).InnerText + ", " + vbNewLine
+                'End If
+
+                ''Obtenemos el Elemento nombre
+                'Dim mNombre = m_node.ChildNodes.Item(4).InnerText
+
+                ''Obtenemos el Elemento apellido
+                'Dim mApellido = m_node.ChildNodes.Item(5).InnerText
+
+                ''Escribimos el resultado en la consola, 
+                ''pero tambien podriamos utilizarlos en
+                ''donde deseemos
+                'MessageBox.Show("Codigo usuario: " & "" _
+                '  & " Nombre: " & mNombre _
+                '  & " Apellido: " & mApellido)
+                'Console.Write(vbCrLf)
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
